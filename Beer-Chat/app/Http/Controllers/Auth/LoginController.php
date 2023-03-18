8ee15a1897f0e161b47d0a816f5d5e8b21cd594a
Slava <sh-e-95@mail.ru>
2023-03-19 01:35:10 +0500

    refactroing login

--- Beer-Chat/app/Http/Controllers/Auth/LoginController.php
+++ Beer-Chat/app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
+use Dotenv\Exception\ValidationException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -22,14 +23,12 @@ class LoginController extends Controller
             'password'=>['required','string'],
         ]);
         if(!Auth::attempt($request->only('email','password'))){
-            return back()
-                ->withInput()
-                ->withErrors([
-                    'email'=> 'These credential don\'t match our records'
-                ]);
+            throw ValidationException::withMessages([
+                'email'=> 'These credential don\'t match our records'
+            ]);
 
         }
 
-        return redirect()->route('messenger');
+        return redirect()->intended(RouteServiceProvider::MESSENGER);
     }
 }
